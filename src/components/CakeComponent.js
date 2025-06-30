83bb428305f2bc4a2476e3abf55e27745eb534bb
dainavi-purav <dainavi.purav@apexon.com>
2025-06-30 19:31:10 +0530

    feat : Created react app Created Action types and Action creators for Cake component Created cake component Created Cake reducer Created store for the application

---
+++ src/components/CakeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeComponent = () => {
+  return (
+    <div>
+      <h2>Namber of cakes</h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+};
+
+export default CakeComponent;
---
+++ src/redux/cake/cakeReducer.js
@@ -0,0 +1,18 @@
+import { BUY_CAKE } from "./cakeTypes";
+const initialState = {
+  cakeCount: 10,
+};
+
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        cakeCount: state.cakeCount - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+export default cakeReducer;
