29947b73cb9e14d8776d29e7b0a9d0676b71e948
dainavi-purav <dainavi.purav@apexon.com>
2025-06-30 22:20:52 +0530

    feat : added component which uses hooks for the action and state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import CakeComponent from "./components/CakeComponent";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import HooksCakeComponent from "./components/HooksCakeComponent";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeComponent />
         <CakeComponent />
       </div>
     </Provider>
---
+++ src/components/HooksCakeComponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { buyCake } from "../redux";
+
+const HooksCakeComponent = () => {
+  const cakeCount = useSelector((state) => {
+    return state.cakeCount;
+  });
+
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Namber of cakes : {cakeCount}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
+    </div>
+  );
+};
+
+export default HooksCakeComponent;
