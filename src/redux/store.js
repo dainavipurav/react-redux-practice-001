a2c652d242fd26b9192a4e0332972d3b0e072a7a
dainavi-purav <dainavi.purav@apexon.com>
2025-07-01 02:05:16 +0530

    feat : Added redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./rootReducer";
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
